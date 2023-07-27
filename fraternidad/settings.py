ac3ef6c5a3214ce6ef3e16c9225c114044dad11e
waltherx <waltermj2012@gmail.com>
2023-07-27 02:54:32 -0400

    allowed host

--- fraternidad/settings.py
+++ fraternidad/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DEBUG")
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["fraternidad.onrender.com", "localhost", "127.0.0.1"]
 
 
 # Application definition
@@ -127,6 +127,7 @@ USE_TZ = True
 STATIC_URL = "static/"
 
 STATIC_ROOT = BASE_DIR / "static"
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
