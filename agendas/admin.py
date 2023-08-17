bf77a0726a9eace721efa70295263cd8b417e9e2
waltherx <waltermj2012@gmail.com>
2023-08-17 15:00:06 -0400

    Update admin.py

--- agendas/admin.py
+++ agendas/admin.py
@@ -27,42 +27,99 @@ admin.site.register(EstadoDeuda)
 
 class AgendaAdmin(admin.ModelAdmin):
     list_display = (
+        "id",
         "fecha",
+        "hora_inicio",
+        "hora_fin",
         "descripcion",
-    )  # Ahora la interfaz mostrará nombre, apellido y email de cada autor.
-    # search_fields = ("fecha")
+        "tipo_evento",
+        "user",
+    )
+    list_display_links = ("id", "user")
+    list_filter = ("tipo_evento",)
+    # list_editable = ("deuda_total", "estado_deuda")
+    search_fields = ("id", "user", "fecha")
+    list_per_page = 25
 
 
 admin.site.register(Agenda, AgendaAdmin)
 
-admin.site.register(Deuda)
+
+class DeudaAdmin(admin.ModelAdmin):
+    list_display = ("id", "deuda_total", "estado_deuda", "user", "created_date")
+    list_display_links = ("id", "user")
+    list_filter = ("estado_deuda",)
+    list_editable = ("deuda_total", "estado_deuda")
+    search_fields = ("id", "user")
+    list_per_page = 25
+
+
+admin.site.register(Deuda, DeudaAdmin)
 admin.site.register(DetalleDeuda)
 admin.site.register(Pago)
 admin.site.register(Qr)
+
+
+class UserTurnoAdmin(admin.ModelAdmin):
+    list_display = ("id", "deuda_total", "estado_deuda", "user", "created_date")
+    list_display_links = ("id", "user")
+    list_filter = ("estado_deuda",)
+    list_editable = ("deuda_total", "estado_deuda")
+    search_fields = ("id", "user")
+    list_per_page = 25
+
+
 admin.site.register(Turno)
+
+
+class UserTurnoAdmin(admin.ModelAdmin):
+    list_display = ("id", "turno", "user")
+    list_display_links = ("id", "turno", "user")
+    list_filter = ("turno",)
+    search_fields = ("turno", "user")
+    list_per_page = 25
+
+
 admin.site.register(UserTurno)
 
 
 class MensualidadAdmin(admin.ModelAdmin):
-    list_display = ("fecha", "monto_fijo", "deuda")  # Ahora la interfaz mostrará
+    list_display = (
+        "id",
+        "fecha",
+        "monto_fijo",
+        "pagado",
+        "deuda",
+    )
+    list_display_links = ("id", "deuda")
+    list_filter = ("pagado",)
+    search_fields = ("fecha", "deuda")
+    list_per_page = 25
+
 
 admin.site.register(Mensualidad, MensualidadAdmin)
+
 
 class ExtraordinariaAdmin(admin.ModelAdmin):
     list_display = (
         "monto",
         "concepto",
-        "created_date"
+        "created_date",
     )  # Ahora la interfaz mostrará nombre, apellido y email de cada autor.
-    #search_fields = ("monto")
-admin.site.register(Extraordinaria,ExtraordinariaAdmin)
+    # search_fields = ("monto")
+
+
+admin.site.register(Extraordinaria, ExtraordinariaAdmin)
+
 
 class DeudaExtraordinariaAdmin(admin.ModelAdmin):
     list_display = (
         "deuda",
         "extraordinaria",
         "pagado",
-        "created_date"
+        "created_date",
     )  # Ahora la interfaz mostrará nombre, apellido y email de cada autor.
-    search_fields = ("deuda")
-admin.site.register(DeudaExtraordinaria,DeudaExtraordinariaAdmin)
+    search_fields = "deuda"
+
+
+admin.site.register(DeudaExtraordinaria, DeudaExtraordinariaAdmin)
