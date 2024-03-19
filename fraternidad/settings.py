efd60938de0e02971644372d684a2afe68b728db
waltherx <waltermj2012@gmail.com>
2024-03-19 01:26:48 -0400

    Update settings.py

--- fraternidad/settings.py
+++ fraternidad/settings.py
@@ -16,7 +16,7 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DEBUG")
 
-ALLOWED_HOSTS = ["18.224,214.77", "127.0.0.1", "localhost"]
+ALLOWED_HOSTS = ["18.224,214.77", "127.0.0.1", "0.0.0.0"]
 
 
 # Application definition
